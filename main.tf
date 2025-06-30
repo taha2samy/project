710cc95e4a9fc677de9943fb084e2b66bb5be58f
Your Name <samyt8435@gmail.com>
2025-06-30 14:18:27 +0300

    Update DynamoDB table name to include random string for uniqueness

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
 
 resource "aws_dynamodb_table" "terraform_locks" {
 
-  name         = "terraform-up-and-running-locks"
+  name         = "terraform-up-and-running-locks-${random_string.prefix.result}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
