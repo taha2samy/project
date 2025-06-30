5fec4fffe807d77c00646b0cbb5ad66ea04d525c
Your Name <samyt8435@gmail.com>
2025-06-30 23:13:38 +0300

    add S3 bucket policy for flow logs and update log destination references

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -1,16 +1,15 @@
-# modules/logging/main.tf
 
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "flow_logs_bucket" {
   bucket = "poc-${var.env}-flow-logs-${data.aws_caller_identity.current.account_id}"
 
-
   tags = {
     Name = "poc-${var.env}-flow-logs-bucket"
     Env  = var.env
   }
 }
+
 resource "aws_s3_bucket_public_access_block" "flow_logs_bucket_public_access" {
   bucket = aws_s3_bucket.flow_logs_bucket.id
 
@@ -28,6 +27,40 @@ resource "aws_s3_bucket_versioning" "flow_logs_bucket_versioning" {
   }
 }
 
+resource "aws_s3_bucket_policy" "allow_flow_logs_write" {
+  bucket = aws_s3_bucket.flow_logs_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSLogDeliveryWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.flow_logs_bucket.arn}/aws-logs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Sid    = "AWSLogDeliveryAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.flow_logs_bucket.arn
+      }
+    ]
+  })
+}
+
+
 resource "aws_cloudwatch_log_group" "flow_logs_log_group" {
   name              = "/aws/vpc/flow-logs-poc-${var.env}"
   retention_in_days = 7
@@ -37,6 +70,7 @@ resource "aws_cloudwatch_log_group" "flow_logs_log_group" {
     Env  = var.env
   }
 }
+
 
 resource "aws_iam_role" "flow_logs_role" {
   name = "poc-${var.env}-flow-logs-role"
@@ -75,29 +109,14 @@ resource "aws_iam_role_policy" "flow_logs_policy" {
           "logs:DescribeLogStreams"
         ]
         Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject"
-        ]
-        Effect   = "Allow"
-        Resource = "${aws_s3_bucket.flow_logs_bucket.arn}/*"
-      },
-      {
-        Action   = "s3:GetBucketAcl"
-        Effect   = "Allow"
-        Resource = aws_s3_bucket.flow_logs_bucket.arn
+        Resource = "*" 
       }
     ]
   })
 }
-
 resource "aws_flow_log" "s3_flow_log" {
   log_destination_type = "s3"
   log_destination      = aws_s3_bucket.flow_logs_bucket.arn
-  iam_role_arn         = aws_iam_role.flow_logs_role.arn
   traffic_type         = "ALL"
   vpc_id               = var.vpc_id
 
@@ -109,8 +128,7 @@ resource "aws_flow_log" "s3_flow_log" {
 
 resource "aws_flow_log" "cloudwatch_flow_log" {
   log_destination_type = "cloud-watch-logs"
-  log_destination      = aws_cloudwatch_log_group.flow_logs_log_group.name
-
+  log_destination      = aws_cloudwatch_log_group.flow_logs_log_group.arn
   iam_role_arn         = aws_iam_role.flow_logs_role.arn
   traffic_type         = "ALL"
   vpc_id               = var.vpc_id
