edb4d200b08944092cecf2951cdf020b8afa7705
Your Name <samyt8435@gmail.com>
2025-07-07 13:50:41 +0300

    test

--- env/pre-prod/key_pair.tf
+++ env/pre-prod/key_pair.tf
@@ -1,5 +1,5 @@
 module "key_pair" {
-  source    = "../modules/key-pair"
+  source    = "../../modules/key-pair"
   key_name  = "pre_prod_ec2_key"
   key_path  = "${path.module}/pre_prod_ec2_key.pem"
   rsa_bits  = 2048
